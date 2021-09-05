38df5a4e72ad6caed6cbf57dd31b83d9063a691a
Parth <parthshah576@gmail.com>
2021-09-05 18:42:42 +0530

    change dir structure

---
+++ des/exec.py
@@ -0,0 +1,10 @@
+from aes import AES
+from des import DES
+
+x = DES(plainText="secretss",key="s3cr3tPy")
+x.encrypt()
+x.decrypt()
+
+y = DES(plainText="hello",key="secretss")
+y.encrypt()
+y.decrypt()
